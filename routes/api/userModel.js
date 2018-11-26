f9f32a15b1e6cab6c5843126735d7a5ac1f41593
Miguel Guimarães <meguim@debian-meguim>
2018-11-26 19:25:58 +0000

    Fix UserModel

--- routes/api/userModel.js
+++ routes/api/userModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var UserSchema =  new Schema({
-    username : {type: String, required: true},
+    username : {type: String, required: true, unique: true},
     user_permissions : {type: String, required: true},
     password : {type: String, required: true},
     email : {type: String}
