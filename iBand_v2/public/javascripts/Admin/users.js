e69366c443315d47fb136ca45bbf0d00dbfcc8bc
anguinho <anguinho95@gmail.com>
2019-01-29 23:05:14 +0000

    More Things

--- iBand_v2/public/javascripts/Admin/users.js
+++ iBand_v2/public/javascripts/Admin/users.js
@@ -1,2 +1,20 @@
+
 $(()=>{
+    
+    $('#create_user_modal').click(()=>{
+        $('#newUserForm').show()
+    })
+
+    // When the user clicks anywhere outside of the modal, close it
+    $('.w3-modal ').click(function(){
+        $('#newUserForm').hide();
+       
+   })                                            
+   $('.w3-modal-content').click(function(e){
+      e.stopPropagation();
+   })                                            
+   
+   $('#close_modal').click(()=>{
+    $('#newUserForm').hide()
+   })
 })
